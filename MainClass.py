2b71301d5d8de2e599d75ed6df3198910313d410
VictorLiu <victorleow@outlook.com>
2018-03-22 14:16:13 +1100

    22/3 14:15 Commit

--- MainClass.py
+++ MainClass.py
@@ -1,4 +1,4 @@
-from ReadBoard import ReadBoard
+#from ReadBoard import ReadBoard
 from ValidMove import ValidMove
 
 print("Enter the input, Ctrl+X to end:")
@@ -12,20 +12,30 @@ while True:
     board.append(line)
 print(board)
 
-if(board[9] == 'Moves'):
+if(board[8] == 'Moves'):
     for i in range(0, 8):
         for j in range(0, 8):
             validMove = ValidMove(i, j, board)
             if(board[i][j] == 'O'):
                 calWhiteMove = validMove.calMoves()
-            elif(board[i][j] == '@'):            
+            elif(board[i][j] == '@'):   
+                        
                 calBlackMove = validMove.calMoves()
             else:
                 continue
 
-    print(calWhiteMove)
-    print(calBlackMove)
-
+    #print(calWhiteMove)
+    #print(calBlackMove)
+'''
 else:
-    elimMove = MakeMoves()
-    fuck this shit, im out of fuking options
+    elimMove = MakeMoves()'''
+'''
+X------X 
+-------- 
+-----O-- 
+----@O-- 
+------O- 
+-----O@- 
+-------@ 
+X------X
+Moves'''
--- ValidMove.py
+++ ValidMove.py
@@ -8,27 +8,41 @@ class ValidMove:
         self.x = x
         self.y = y
         self.board = board
-    
+    '''   x 上下  y 左右'''
     def calMoves(self):
         
-        if(self.board[self.x-1][self.y] == '-'):
-            self.validMoves += 1
-        elif(self.board[self.x-2][self.y] == '-'):
-            self.validMoves += 1
-        
-        if(self.board[self.x+1][self.y] == '-'):
-            self.validMoves += 1
-        elif(self.board[self.x+2][self.y] == '-'):
-            self.validMoves += 1
-        
-        if(self.board[self.x][self.y-1] == '-'):
-            self.validMoves += 1
-        elif(self.board[self.x][self.y-2] == '-'):
-            self.validMoves += 1        
-        
-        if(self.board[self.x][self.y+1] == '-'):
-            self.validMoves += 1
-        elif(self.board[self.x][self.y+2] == '-'):
-            self.validMoves += 1
+        try: 
+            if(self.board[self.x-1][self.y] == '-'):
+                self.validMoves += 1
+            elif(self.board[self.x-2][self.y] == '-') :
+                self.validMoves += 1
+            
+            if(self.board[self.x+1][self.y] == '-'):
+                self.validMoves += 1
+            elif(self.board[self.x+2][self.y] == '-') :
+                
+                print (self.board[self.x+2][self.y])   
+                print (self.x+2, self.y)
+                print (self.board)
+                self.validMoves += 1
+                #print ("HHHHHH2333\n") 
+            
+            if(self.board[self.x][self.y-1] == '-'):
+                self.validMoves += 1
+            elif(self.board[self.x][self.y-2] == '-') :
+                print (self.board[self.x][self.y])   
+                print (self.x, self.y)
+                print (self.board)
+                
+
+
+                self.validMoves += 1        
+            
+            if(self.board[self.x][self.y+1] == '-'):
+                self.validMoves += 1
+            elif(self.board[self.x][self.y+2] == '-') and self.y < 6:
+                self.validMoves += 1
+        except IndexError:
+            print ("Out of bounds")
 
         return self.validMoves
